d5e045cda6098082ade9183e229a920275cca07d
leandro <leandro.ecabello@gmail.com>
2020-12-24 17:53:02 -0300

    Add try/catch in twilio controllers

--- Back/src/app/controllers/twilio.controller.js
+++ Back/src/app/controllers/twilio.controller.js
@@ -1,13 +1,30 @@
-const Twilio = require("../libs/twilio");
+const { tokenGenerate, roomCreate } = require('../libs/twilio');
 
-class TwilioController {
-  static async getToken(req, res) {
-    return Twilio.tokenGenerate(req, res);
-  }
+const TwilioController = {
+  getToken: async (req, res) => {
+    try {
+      const token = tokenGenerate();
+      console.log(token);
+      res.status(200).json({ token });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Internal Server Error', error: err });
+    }
+  },
 
-  static async addRoom(req, res) {
-    return await Twilio.roomCreate(req, res);
-  }
-}
+  addRoom: async (req, res) => {
+    try {
+      const room = await roomCreate(req, res);
+      console.log(room);
+      res.status(201).json({ room });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: 'An existing Room exists with the same name',
+        error: err,
+      });
+    }
+  },
+};
 
 module.exports = TwilioController;
