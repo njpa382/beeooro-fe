edd76fbf8a85f2e27fc6e1d1c57deefacf97fb14
leandro <leandro.ecabello@gmail.com>
2020-12-24 19:31:59 -0300

    Modify addRoom controller

--- Back/src/app/controllers/twilio.controller.js
+++ Back/src/app/controllers/twilio.controller.js
@@ -4,21 +4,18 @@ const TwilioController = {
   getToken: async (req, res) => {
     try {
       const token = tokenGenerate();
-      console.log(token);
       res.status(200).json({ token });
     } catch (err) {
-      console.log(err);
       res.status(500).json({ message: 'Internal Server Error', error: err });
     }
   },
 
   addRoom: async (req, res) => {
     try {
-      const room = await roomCreate(req, res);
-      console.log(room);
+      const { uniqueName } = req.body;
+      const room = await roomCreate(uniqueName);
       res.status(201).json({ room });
     } catch (err) {
-      console.log(err);
       res.status(400).json({
         message: 'An existing Room exists with the same name',
         error: err,
